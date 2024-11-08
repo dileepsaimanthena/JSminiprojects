406189fbdb66a0bf75b5ab82671dcd7ff6b9c88a
dileepsai4930 <dileepsaimanthena4930@gmail.com>
2024-11-08 13:43:47 +0530

    error handling

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -4,12 +4,21 @@ const cityName = document.getElementById('cityName')
 const temp = document.getElementById('temperature')
 const time = document.getElementById('time')
 async function getData(cityName){
-    const result = await fetch(`http://api.weatherapi.com/v1/current.json?key=67f0b70a44dd4d628d674957240811&q=${cityName}&aqi=yes`)
-    return await result.json()
+    try{
+        const result = await fetch(`http://api.weatherapi.com/v1/current.json?key=67f0b70a44dd4d628d674957240811&q=${cityName}&aqi=yes`)
+        return await result.json()
+    }
+    catch(error){
+        cityName.innerText = 'Something went wrong';
+    }
 }
 btn.addEventListener('click',async()=>{
-    const result = await getData(city.value)
-    cityName.innerText = `${result.location.country},${result.location.region}-${result.location.name}`
-    temp.innerText = `${result.current.temp_c}°C`
-    time.innerText = `${result.location.localtime}`
+    try{
+        const result = await getData(city.value)
+        cityName.innerText = `${result.location.country},${result.location.region}-${result.location.name}`
+        temp.innerText = `${result.current.temp_c}°C`
+        time.innerText = `${result.location.localtime}`
+    }catch(error){
+        cityName.innerText = 'Please enter city name'
+    }
 })
